576d39c96806ed66e4c3521d30abfebe048682a2
王礼壮 <101847940+lazyCookies@users.noreply.github.com>
2022-07-22 11:30:25 +0800

    Delete my-gulp directory

--- my-gulp/src/index.js
+++
@@ -1,7 +0,0 @@
-var xhr = new XMLHttpRequest()
-xhr.open('GET','https://fanyi.baidu.com/mtpe/user/getInfo?_=1649235558573')
-xhr.send()
-xhr.onload = function () {
-    console.log(xhr.responseText)
-    console.log(111)
-}
